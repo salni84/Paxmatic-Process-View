6ff6937093535aa8160c0aaa535188d8ac2d621a
salni84 <roberto.salerno@swisscom.com>
2021-02-12 22:42:29 +0100

    server-updates

--- Server/events.js
+++ Server/events.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+
 
 function createRouter(db) {
     const router = express.Router();
+    const app = express();
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(function (req, res) {
+        res.end(JSON.stringify(req.body, null, 2))
+    });
+
+
 
     router.get('/basic', function (req, res, next) {
         db.query(
-            'SELECT * FROM process',
+            'SELECT * FROM basicprocess order by position',
             (error, results) => {
                 if (error) {
                     console.log(error);
@@ -17,8 +27,58 @@ function createRouter(db) {
         );
     });
 
-    return router
 
+    router.delete('/basic/:id', function (req, res) {
+        db.query(
+            'DELETE FROM basicprocess WHERE id = ?', [req.params.id],
+            (error,results) =>  {
+                console.log(results)
+                if (error) {
+                    res.status(500).json({status: 'error'});
+                }else {
+                    res.status(200).json(results)
+                }
+            }
+        )
+    });
+
+    router.post('/basic/new', (req, res) => {
+
+            let level = req.body.level;
+            let name = req.body.name;
+            let color = req.body.color;
+            let form = req.body.form;
+            let position = req.body.position;
+
+            db.query(
+                'INSERT INTO basicprocess VALUES (?,?,?,?,?,?)', [null, level, name, color, form, position],
+                (error, results) => {
+                    if (error) {
+                        console.log(error);
+                        res.status(500).json({status: 'error'});
+                    }else {
+                        res.status(200).json(results)
+                    }
+            });
+    });
+
+
+
+    router.put('/basic', (req, res) => {
+
+        for (let x = 0; x < req.body.length; x++) {
+            let id = req.body[x].id;
+            let position = req.body[x].position;
+
+            db.query(
+                "UPDATE basicprocess SET position = ? WHERE id = ?", [position, id],
+                () => {})}
+
+            res.status(200).json()
+    });
+
+
+    return router
 }
 
 module.exports = createRouter;
