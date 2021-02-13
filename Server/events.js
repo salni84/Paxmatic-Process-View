6e19177beeea464a276efbd837c6944d09bafd09
salni84 <roberto.salerno@swisscom.com>
2021-02-13 11:30:10 +0100

    sublevel

--- Server/events.js
+++ Server/events.js
@@ -13,7 +13,7 @@ function createRouter(db) {
 
 
 
-    router.get('/basic', function (req, res, next) {
+    router.get('/basic', function (req, res) {
         db.query(
             'SELECT * FROM basicprocess order by position',
             (error, results) => {
@@ -26,6 +26,23 @@ function createRouter(db) {
             }
         );
     });
+
+    router.get('/sub/:parent', function (req, res) {
+        console.log(req.body.parent)
+        db.query(
+            'SELECT * FROM subprocess WHERE parent = ? order by position', [req.params.parent],
+            (error, results) => {
+                console.log(results)
+                if (error) {
+                    console.log(error);
+                    res.status(500).json({status: 'error'});
+                }else {
+                    res.status(200).json(results)
+                }
+            }
+        );
+    });
+
 
 
     router.delete('/basic/:id', function (req, res) {
@@ -42,6 +59,23 @@ function createRouter(db) {
         )
     });
 
+
+    router.delete('/sub/:id', function (req, res) {
+        db.query(
+            'DELETE FROM subprocess WHERE id = ?', [req.params.id],
+            (error,results) =>  {
+                console.log(results)
+                if (error) {
+                    res.status(500).json({status: 'error'});
+                }else {
+                    res.status(200).json(results)
+                }
+            }
+        )
+    });
+
+
+
     router.post('/basic/new', (req, res) => {
 
             let level = req.body.level;
@@ -49,9 +83,10 @@ function createRouter(db) {
             let color = req.body.color;
             let form = req.body.form;
             let position = req.body.position;
+            let parent = '';
 
             db.query(
-                'INSERT INTO basicprocess VALUES (?,?,?,?,?,?)', [null, level, name, color, form, position],
+                'INSERT INTO basicprocess VALUES (?,?,?,?,?,?,?)', [null, level, name, color, form, position, parent],
                 (error, results) => {
                     if (error) {
                         console.log(error);
@@ -62,6 +97,27 @@ function createRouter(db) {
             });
     });
 
+
+    router.post('/sub/new', (req, res) => {
+
+        let level = req.body.level;
+        let name = req.body.name;
+        let color = req.body.color;
+        let form = req.body.form;
+        let position = req.body.position;
+        let parent = req.body.parent;
+
+        db.query(
+            'INSERT INTO subprocess VALUES (?,?,?,?,?,?,?)', [null, level, name, color, form, position, parent],
+            (error, results) => {
+                if (error) {
+                    console.log(error);
+                    res.status(500).json({status: 'error'});
+                }else {
+                    res.status(200).json(results)
+                }
+            });
+    });
 
 
     router.put('/basic', (req, res) => {
@@ -75,6 +131,19 @@ function createRouter(db) {
                 () => {})}
 
             res.status(200).json()
+    });
+
+    router.put('/sub', (req, res) => {
+
+        for (let x = 0; x < req.body.length; x++) {
+            let id = req.body[x].id;
+            let position = req.body[x].position;
+
+            db.query(
+                "UPDATE subprocess SET position = ? WHERE id = ?", [position, id],
+                () => {})}
+
+        res.status(200).json()
     });
 
 
